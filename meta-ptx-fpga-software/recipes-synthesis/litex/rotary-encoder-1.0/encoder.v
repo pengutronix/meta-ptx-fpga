432e1484b5477333b9aa23a045f0e99f037e8ab2
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2023-01-09 07:06:36 +0100

    fpga-software: synthesis: rotary: fix indentation
    The indentation is completely b0rken. Fix this.
    
    (PS: sadly, there is only one verilog-mode for the *best* editor out
    there, but this mode does some really stupid sh^wstuff sometimes.
    Luckily, it is the only mode, that is this stupid.)
    
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- meta-ptx-fpga-software/recipes-synthesis/litex/rotary-encoder-1.0/encoder.v
+++ meta-ptx-fpga-software/recipes-synthesis/litex/rotary-encoder-1.0/encoder.v
@@ -1,20 +1,20 @@
 // based on Quad_compact from https://shadowcode.io/quadrature-decoder-verilog/
 
 module rotary_encoder(
-		      input						 clk,
-		      input						 A,
-		      input						 B,
-		      input						 reset,
-		      input wire [4:0] din,
-		      output wire [4:0] dout,
-		      output reg			 direction,
-					output wire			 update
-		      );
+                      input             clk,
+                      input             A,
+                      input             B,
+                      input             reset,
+                      input wire [4:0]  din,
+                      output wire [4:0] dout,
+                      output reg        direction,
+                      output wire       update
+                      );
 
-    reg [1:0]				       sync, AB;
-    reg [1:0]				       cs;
-    wire [1:0]				       tmp, ns;
-    reg [6:0]	signal;
+    reg [1:0] 		sync, AB;
+    reg [1:0]       cs;
+    wire [1:0]      tmp, ns;
+    reg [6:0]       signal;
 
     // sync/debounce
     always @(posedge clk) begin
@@ -26,25 +26,25 @@ module rotary_encoder(
     assign tmp = {AB[1], AB[1] ^ AB[0]};
 
     assign ns = tmp - cs;
-		assign update = ns[0];
+    assign update = ns[0];
 
+    // the encoder we use, generates 4 pulses per "click".
     assign dout = signal >> 2;
 
     always @(posedge clk) begin
         if(reset) begin
-						signal <= din;
-						cs <= 0;
-        end else
-						if(ns[0] == 1'b1)
-                begin
-										cs <= cs + ns; // set current state to old state
-										if (ns[1] == 1'b1) begin
-                        signal <= signal - 1'b1;
-												direction <= 1'b0;
-										end else begin
-                        signal <= signal + 1'b1;
-												direction <= 1'b1;
-										end
-								end
+            signal <= din;
+            cs <= 0;
+        end else if(ns[0] == 1'b1)
+            begin
+                cs <= cs + ns; // set current state to old state
+                if (ns[1] == 1'b1) begin
+                    signal <= signal - 1'b1;
+                    direction <= 1'b0;
+                end else begin
+                    signal <= signal + 1'b1;
+                    direction <= 1'b1;
+                end
+            end
     end
 endmodule
